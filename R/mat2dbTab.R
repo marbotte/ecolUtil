979dce4c456fdfa8c0061d546ad06ccbbf07a6c4
Marius Bottin <bottinmarius@gmail.com>
2018-05-16 11:48:18 -0500

    Mode checklist added to the functions dbTab2mat and mat2dbTab

--- R/mat2dbTab.R
+++ R/mat2dbTab.R
@@ -1,8 +1,14 @@
 mat2dbTab <-
-function(mat)
+function(mat,checklist=F)
 {
   W<-which(mat>0,arr.ind<-T)
+	if(!checklist){
   dbTab<-data.frame(SU=rownames(mat)[W[,"row"]],sp=colnames(mat)[W[,"col"]],ab=mat[W])
+	}else{
+  dbTab<-data.frame(SU=rownames(mat)[W[,"row"]],sp=colnames(mat)[W[,"col"]])
+	}
+	numSU<-all(grepl("^[0-9]+$",dbTab$SU))
+	if(numSU){dbTab$SU<-as.numeric(as.character(dbTab$SU))}
   dbTab<-dbTab[order(dbTab$SU,dbTab$sp),]
   return(dbTab)
 }
