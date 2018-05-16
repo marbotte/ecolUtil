317fd5d4bcda788fdbadf539f4d4261bc536f120
Marius Bottin <bottinmarius@gmail.com>
2018-05-16 15:19:07 -0500

    bug corrected in the reduceTable function

--- R/reduceTable.R
+++ R/reduceTable.R
@@ -1,7 +1,7 @@
 reduceTable <-
 function(tab,sumRowMin=5,sumRowMax=NA,sumColMin=3,sumColMax=NA,uniq=T,trace=T)
 {
-if(trace){cat("start dim",dim(table),"\n")}
+if(trace){cat("start dim",dim(tab),"\n")}
 nbPass<-0
 suppression<-T
 suppRowMin<-suppRowMax<-suppColMin<-suppColMax<-dupl<-F
@@ -11,17 +11,17 @@ if(trace){
 nbPass<-nbPass+1
 cat("pass n° ",nbPass,"\n")
 }
-if(!is.na(sumRowMin)){suppRowMin<-rowSums(table)<sumRowMin}
-if(!is.na(sumRowMax)){suppRowMax<-rowSums(table)>sumRowMax}
-if(!is.na(sumColMin)){suppColMin<-colSums(table)<sumColMin}
-if(!is.na(sumColMax)){suppColMax<-colSums(table)>sumColMax}
-if(uniq){dupl<-duplicated(table)}
+if(!is.na(sumRowMin)){suppRowMin<-rowSums(tab)<sumRowMin}
+if(!is.na(sumRowMax)){suppRowMax<-rowSums(tab)>sumRowMax}
+if(!is.na(sumColMin)){suppColMin<-colSums(tab)<sumColMin}
+if(!is.na(sumColMax)){suppColMax<-colSums(tab)>sumColMax}
+if(uniq){dupl<-duplicated(tab)}
 if(as.logical(sum(suppRowMin,suppRowMax,suppColMin,suppColMax,dupl)))
-{table<-table[(!suppRowMin)&(!suppRowMax)&(!dupl),(!suppColMin)&(!suppColMax)]
+{tab<-tab[(!suppRowMin)&(!suppRowMax)&(!dupl),(!suppColMin)&(!suppColMax)]
 }else
 {suppression<-F
 }
 }
-if(trace){cat("final dim",dim(table),"\n")}
-return(table)
+if(trace){cat("final dim",dim(tab),"\n")}
+return(tab)
 }
