6b5368319ecec3e4b1f26fbfcbead2506c8bd99f
Marius Bottin <bottinmarius@gmail.com>
2018-06-26 10:33:17 -0500

    added the PN centrality function for valued networks

---
+++ R/pnCentrality_val.R
@@ -0,0 +1,7 @@
+pnCentrality_val<-function(posit,negat,fac_beta=max(c(posit,negat))*2)
+{
+	stopifnot(nrow(posit)==ncol(posit),nrow(posit)==nrow(negat),nrow(negat)==ncol(negat),any(!posit<0),any(negat<0))
+	A<-posit-(2*negat)
+	n<-nrow(posit)
+	return(solve(diag(n)-((1/(fac_beta*n-fac_beta))*A))%*%rep(1,n))
+}
